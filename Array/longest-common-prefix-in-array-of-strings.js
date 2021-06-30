4a8df10b3776fb9bab5a9fb5b7db21356f41a354
Praveen Mistry <praveensuthar108@gmail.com>
2021-06-30 09:48:32 +0530

    Added longest common prefix
    with DAC

--- Array/longest-common-prefix-in-array-of-strings.js
+++ Array/longest-common-prefix-in-array-of-strings.js
@@ -33,3 +33,39 @@ function longestCommonPrefix(){
 }
 
 longestCommonPrefix();
+
+// DAC
+
+let arr = ["javasctipt", "jav2a", "javateam", "jadu"];
+let len = arr.length;
+let l=0;
+let r= len-1;
+
+function findPrefixUntill(str1, str2){
+	console.log("length",str2, typeof str2)
+	let n1 = str1.length;
+	let n2 = str2.length;
+	let result = "";
+	for (let i = 0, j = 0; i <= n1 - 1 &&
+                j <= n2 - 1; i++, j++) {
+		if(str1[i]==str2[j]){
+			result += str1[i];
+		}
+	}
+	return result;
+}
+
+function findPrefix(arr,l,r){
+	if(l===r){
+		return arr[l]
+	}
+	if(l<r){
+		let mid = Math.ceil(l+(r-l)/2);
+		let str1 = findPrefix(arr,l,mid-1);
+		let str2 = findPrefix(arr,mid,r);
+		return	findPrefixUntill(str1,str2);
+	}
+	return null;
+}
+
+findPrefix(arr,l,r);
